96b893dd0dd875bf42821613407db4218ef348fd
vnSasa <ol.yaremechko@gmail.com>
2022-11-12 07:36:00 +0200

    added validation for login

--- pkg/repository/auth_postgres.go
+++ pkg/repository/auth_postgres.go
@@ -1,7 +1,9 @@
 package repository
 
 import (
-	"github.com/sirupsen/logrus"
+	"strings"
+	"errors"
+	"strconv"
 	"github.com/vnSasa/IntelliasGo_Project_REST-API_Film-Manager"
 	"fmt"
 	"github.com/jmoiron/sqlx"
@@ -27,15 +29,34 @@ func (r *AuthPostgres) CreateAdmin(admin app.User) (int, error) {
 
 func (r *AuthPostgres) CreateUser(user app.User) (int, error) {
 	var id int
+	err := r.validLogin(user.Login)
+	if err != nil {
+		return 0, errors.New(fmt.Sprintf("your enter data for login is not valid: %s", err))
+	}
 	query := fmt.Sprintf("INSERT INTO %s (login, password_hash, age) values ($1, $2, $3) RETURNING id", usersTable)
-	if len(user.Login) != 10 {
-		logrus.Print("not phone number")
-	} 
 	row := r.db.QueryRow(query, user.Login, user.Password, user.Age)
 	if err := row.Scan(&id); err != nil {
 		return 0, err
 	}
 	return id, nil
+}
+
+func (r *AuthPostgres) validLogin(login string) error {
+	if len(login) != 10 {
+		return errors.New("must have 10 numbers")
+	}
+	_, err := strconv.Atoi(login)
+	if err != nil {
+		return errors.New(fmt.Sprintf("your enter data have not only numbers: %s", login))
+	}
+	loginSlice := strings.Split(login, "")
+	for i := 0; i < len(loginSlice); i++ {
+		x, _ := strconv.Atoi(loginSlice[0])
+		if x != 0 {
+			return errors.New(fmt.Sprintf("phone number must have strart from 0, but you have: %d", x))
+		}
+	}
+	return nil
 }
 
 func (r *AuthPostgres) DeleteUser(user app.User) (int, error) {
